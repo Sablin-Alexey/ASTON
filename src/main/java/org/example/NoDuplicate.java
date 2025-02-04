865cba6a0eaca792e00b728a7a39a3036f4c0772
Sablin Alexey <asablin8595439@gmail.com>
2025-02-04 09:57:11 +0300

    2 Добавил вывод повторений по кааждому слову

--- src/main/java/org/example/NoDuplicate.java
+++ src/main/java/org/example/NoDuplicate.java
@@ -7,14 +7,19 @@ public class NoDuplicate {
     public static Set<String> antiduplicate(String[] collection) {
         Set<String> noDuplicates = new HashSet<>();
         int size = 0;
-        int num = 0;
         for (int i = 0; i < collection.length; i++) {
+            int num = 0;
             noDuplicates.add(collection[i]);
             if (size < noDuplicates.size()) {
                 size = noDuplicates.size();
-            } else num++;
+                for (int j = 0; j < collection.length; j++) {
+                    if (collection[i].equals(collection[j])) {
+                        num++;
+                    }
+                }
+                System.out.println("Слово " + collection[i] + " повторяется " + num + " раз(а).");
+            }
         }
-        System.out.println(num + " - количество дублированных слов");
         return noDuplicates;
     }
 }
